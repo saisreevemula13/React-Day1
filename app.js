de5258ff6e8440c4a74a373034668b1c36a7586e
shivaram235vemula <shivaram235vemula@gmail.com>
2023-07-13 21:28:20 +0530

    adding readme file

--- app.js
+++ app.js
@@ -13,22 +13,107 @@
 // console.log(greeting);
 // console.log(x);
 
-const division = React.createElement(
-  "div",
-  {},
-  React.createElement("h1", {}, React.createElement("span", {}, "I'm Saisree"))
-);
-var root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(division);
+// const division = React.createElement(
+//   "div",
+//   {id:child1},
+//   React.createElement("h1", {}, React.createElement("span", {}, "I'm Saisree"))
+// );
+// var root = ReactDOM.createRoot(document.getElementById("container"));
+// root.render(division);
 
-const division2 = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "I'm from second child2 div h1 tag")
-  )
-);
-var root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(division2);
+// const division2 = React.createElement(
+//   "div",
+//   {},
+//   React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "I'm from second child2 div h1 tag")
+//   )
+// );
+// var root = ReactDOM.createRoot(document.getElementById("container"));
+// root.render(division2);
+
+// const city = "Hyd";
+// function helloName() {
+//   var name = "ss";
+//   let x = 22;
+
+//   const city = "knr";
+//   city = "khamam";
+// }
+// console.log(city);
+// helloName();
+
+
+//<div id="parent">
+//     <div id="parent1">
+//         <h1 id="child11">
+//              <span class="one-one">sample text</span>
+//          </h1>
+//          <h1 id="child12">
+//              <span class="one-two">sample text</span>
+//          </h1>
+//          </div>
+//     <div id="parent2">
+//         <div id="two-one">
+//               <p id="child2">React child2</p>
+//               <p id="child1">React child1</p>
+//         </div>
+//          <div id="two-two">
+//               <p id="child2">React child2</p>
+//               <p id="child1">React child1</p>
+//         </div>
+//     </div>
+//</div>
+//Assigment
+const assignment = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "parent1" }, [
+    React.createElement(
+      "h1",
+      { id: "child11" },
+      React.createElement("span", { className: "one-one" }, "sample text")
+    ),
+    React.createElement(
+      "h1",
+      { id: "child12" },
+      React.createElement("span", { className: "one-two" }, "sample text")
+    ),
+  ]),
+  React.createElement("div", { id: "parent2" }, [
+    React.createElement("div", { id: "two-one" }, [
+      React.createElement("p", { id: "child2" }, "React child2"),
+      React.createElement("p", { id: "child1" }, "React child1"),
+    ]),
+    React.createElement("div", { id: "two-two" }, [
+      React.createElement("p", { id: "child2" }, "React child2"),
+      React.createElement("p", { id: "child1" }, "React child1"),
+    ]),
+  ]),
+]);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(assignment);
+//Example 1
+/* <div id="container">
+<div id="parent">
+  <div id="child1">
+    <h1>Hello</h1>
+    <h1>Saisree</h1>
+  </div>
+  <div id="child2">
+    <h1>Hello</h1>
+    <h1>Saisree</h1>
+  </div>
+</div>
+</div> */
+// const division = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm Saisree"),
+//     React.createElement("h2", {}, "I'm Saisree"),
+//   ]),
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm Saisree"),
+//     React.createElement("h2", {}, "I'm Saisree"),
+//   ]),
+// ]);
+// var root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(division);
---
+++ readme.md
@@ -0,0 +1,8 @@
+1.cdn links used to import react- code in CDN is js
+2.React is a JS library, Facebook has written this code.
+3. i)First link in CDN-React Core react.development.js--y we use---this has React algorithm
+   ii)--Browser understand using React.Dom(bridge b/w react & browser used for DOM opeerations)- DCN-link-2
+4.React is not specified to browser- mobile phone browsers & 3D features building application.
+5.Angular js only for web app.
+6.CDN links should be in order- 1st-react then reactDOM--means react in our prjt.
+7.
